ceb975a2be46de42da25474d4bbf176cc10b0e2c
Rebecca Hansson <rebeccahansson2016@gmail.com>
2024-05-20 16:04:51 +0200

    heroblock styling

--- client/src/components/HeroBlock.tsx
+++ client/src/components/HeroBlock.tsx
@@ -10,20 +10,31 @@ export const HeroBlock = () => {
       <div className="container">
         <div className="html-box">
           <img className="hero-logo" src={htmlLogo} alt="HTML Logo" />
-          <h3>Html5</h3>
-          <p>Lite text om html.</p>
+          <h2>Html5</h2>
+          <p>
+            HTML5 is a markup language used for structuring presenting content
+            on the World Wide Web. It is the fifth and current major version of
+            the HTML standard.
+          </p>
           <button className="common-btn">Read more</button>
         </div>
         <div className="css-box">
           <img className="hero-logo" src={cssLogo} alt="HTML Logo" />
-          <h3>CSS3</h3>
-          <p>Lite text om CSS.</p>
+          <h2>CSS3</h2>
+          <p>
+            Cascading Style Sheets is a style sheet language used for describing
+            the presentation of a document written in a markup language.
+          </p>
           <button className="common-btn">Read more</button>
         </div>
         <div className="js-box">
           <img className="hero-logo" src={jsLogo} alt="HTML Logo" />
-          <h3>JS</h3>
-          <p>Lite text om javascript</p>
+          <h2>JS</h2>
+          <p>
+            JavaScript, often abbreviated as JS, is a high-level, dynamic,
+            weakly typed, prototype-based, multi-paradigm, and interpreted
+            programming language.{" "}
+          </p>
           <button className="common-btn">Read more</button>
         </div>
       </div>
