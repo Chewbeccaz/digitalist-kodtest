b53cb5029e811afbc12e868960dc6994f5051b23
Rebecca Hansson <rebeccahansson2016@gmail.com>
2024-05-22 12:03:41 +0200

    clean up and slow down animation

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -5,11 +5,6 @@ import { Searchbox } from "./Searchbox";
 
 export const Navbar = () => {
   const Logo = "/svgs/digitalist_logo.svg";
-  // const [isSearchOpen, setIsSearchOpen] = useState(false);
-
-  // const toggleSearchBox = () => {
-  //   setIsSearchOpen(!isSearchOpen);
-  // };
 
   return (
     <nav>
@@ -18,17 +13,10 @@ export const Navbar = () => {
       </div>
       <div className="icons">
         <Searchbox />
-
-        {/* <div className="search-box">
-          <button onClick={toggleSearchBox}>
-            <IoMdSearch />
-          </button>
-        </div> */}
         <button>
           <IoMenuOutline />
         </button>
       </div>
-      {/* {isSearchOpen && <Searchbox isOpen={isSearchOpen} />} */}
     </nav>
   );
 };
